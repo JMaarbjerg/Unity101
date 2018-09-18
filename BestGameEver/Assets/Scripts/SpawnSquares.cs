0d0027eae5c64bf4200f5d5f6768f792084b5001
JMAA <jacobmc@gmail.com>
2018-09-18 09:00:58 +0200

    Spawnscript begun

---
+++ BestGameEver/Assets/Scripts/SpawnSquares.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnSquares : MonoBehaviour {
+
+    public GameObject parallel;
+    public GameObject corner;
+
+	// Use this for initialization
+	void Start () {
+        Instantiate(parallel);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
