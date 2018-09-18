f0afad70da34e307a769aef53b6909f71d1b5049
JMAA <jacobmc@gmail.com>
2018-09-18 10:57:55 +0200

    Quaternions, Quaternions, & Quaternions
    Quaternions er svære at dreje sit hjerne om...

--- BestGameEver/Assets/Scripts/SpawnSquares.cs
+++ BestGameEver/Assets/Scripts/SpawnSquares.cs
@@ -6,11 +6,37 @@ public class SpawnSquares : MonoBehaviour {
 
     public GameObject parallel;
     public GameObject corner;
+    public int[] anglePool = { 0, 180, 90, 270 };
+    public Quaternion newAngle = Quaternion.identity;
 
 	// Use this for initialization
 	void Start () {
-        Instantiate(parallel);
-	}
+        for (int x = 0; x <= 60; x = x+12) {
+            float rnd = Random.value;
+            if(rnd < 0.66)
+            {                
+                int i = Random.Range(0, 3);
+                Debug.Log(i);
+                newAngle.y = anglePool[i];
+                newAngle = new Quaternion(newAngle.x, 90, newAngle.z, newAngle.w);
+                Debug.Log(newAngle.y);
+                Instantiate(corner, new Vector3(x, 0, 0), newAngle);
+            }
+            
+            else
+            {
+                int i = Random.Range(0, 1);
+                newAngle.y = anglePool[i];
+                Instantiate(parallel, new Vector3(x, 0, 0), newAngle);
+            }
+        }
+        /*
+        Debug.Log(anglePool[0]);
+        Debug.Log(anglePool[1]);
+        Debug.Log(anglePool[2]);
+        Debug.Log(anglePool[3]);
+        */  
+    }
 	
 	// Update is called once per frame
 	void Update () {
