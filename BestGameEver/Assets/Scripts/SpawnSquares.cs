ae4df9b3a2c78b081e97dfcde6ab8dc36590e075
JMAA <jacobmc@gmail.com>
2018-09-18 13:31:02 +0200

    Working algorithm
    Scriptet er ændret så der ikke benyttes Quarterions længere. Der findes istedet én af de nu seks (før kun to) prefabs.

--- BestGameEver/Assets/Scripts/SpawnSquares.cs
+++ BestGameEver/Assets/Scripts/SpawnSquares.cs
@@ -4,42 +4,38 @@ using UnityEngine;
 
 public class SpawnSquares : MonoBehaviour {
 
-    public GameObject parallel;
-    public GameObject corner;
-    public int[] anglePool = { 0, 180, 90, 270 };
-    public Quaternion newAngle = Quaternion.identity;
+    public GameObject[] prefabs;
+    private GameObject[] clones;
 
-	// Use this for initialization
-	void Start () {
-        for (int x = 0; x <= 60; x = x+12) {
-            float rnd = Random.value;
-            if(rnd < 0.66)
-            {                
-                int i = Random.Range(0, 3);
-                Debug.Log(i);
-                newAngle.y = anglePool[i];
-                newAngle = new Quaternion(newAngle.x, 90, newAngle.z, newAngle.w);
-                Debug.Log(newAngle.y);
-                Instantiate(corner, new Vector3(x, 0, 0), newAngle);
-            }
-            
-            else
-            {
-                int i = Random.Range(0, 1);
-                newAngle.y = anglePool[i];
-                Instantiate(parallel, new Vector3(x, 0, 0), newAngle);
-            }
-        }
-        /*
-        Debug.Log(anglePool[0]);
-        Debug.Log(anglePool[1]);
-        Debug.Log(anglePool[2]);
-        Debug.Log(anglePool[3]);
-        */  
+    // Use this for initialization
+    void Start () {
+        DrawSquares();        
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (Input.anyKey)
+        {
+            clones = GameObject.FindGameObjectsWithTag("Clone");
+            for(int i = 0; i < clones.Length; i++)
+            {
+                Destroy(clones[i]);
+            }
+
+            DrawSquares();
+        }
 	}
+
+    void DrawSquares()
+    {
+        for (int x = 0; x <= 120; x = x + 12)
+        {
+            for (int z = 0; z <= 48; z = z + 12)
+            {
+                int i = Random.Range(0, 5);
+                Instantiate(prefabs[i], new Vector3(x, 0, z), Quaternion.identity);
+
+            }
+        }
+    }
 }
